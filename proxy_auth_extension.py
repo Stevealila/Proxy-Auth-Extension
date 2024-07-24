22df806508ff2505a566f109f594d85cb87eaf53
Stevealila <stevealila25@gmail.com>
2024-07-24 13:14:45 +0300

    Add proxy file

---
+++ proxy_auth_extension.py
@@ -0,0 +1,60 @@
+import zipfile
+
+def create_proxy_auth_extension(proxy_host, proxy_port, proxy_username, proxy_password):
+    manifest_json = """
+    {
+        "version": "1.0.0",
+        "manifest_version": 2,
+        "name": "Proxy Auth Extension",
+        "permissions": [
+            "proxy",
+            "tabs",
+            "unlimitedStorage",
+            "storage",
+            "<all_urls>",
+            "webRequest",
+            "webRequestBlocking"
+        ],
+        "background": {
+            "scripts": ["background.js"]
+        }
+    }
+    """
+
+    background_js = """
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+            singleProxy: {
+                scheme: "http",
+                host: "%s",
+                port: %d
+            },
+            bypassList: ["localhost"]
+        }
+    };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    function callbackFn(details) {
+        return {
+            authCredentials: {
+                username: "%s",
+                password: "%s"
+            }
+        };
+    }
+
+    chrome.webRequest.onAuthRequired.addListener(
+        callbackFn,
+        {urls: ["<all_urls>"]},
+        ['blocking']
+    );
+    """ % (proxy_host, proxy_port, proxy_username, proxy_password)
+
+    extension = 'proxy_auth_extension.zip'
+    with zipfile.ZipFile(extension, 'w') as zp:
+        zp.writestr("manifest.json", manifest_json)
+        zp.writestr("background.js", background_js)
+    
+    return extension
